24670f5b1994129e5c014a5704047d14888e1edb
kai.hwang@gmail.com <kaihwang@nx5.priv>
2014-11-14 15:12:45 -0800

    use 3dDeconvolve.

--- Optimization/test_rand_timing.sh
+++ Optimization/test_rand_timing.sh
@@ -1,16 +1,22 @@
 #!/bin/bash/
 
 # script to create stimulus timing. 
+cd /home/despo/kaihwang/bin/TDSigEI-Paradigm/Optimization
 
 optseq2 \
---ntp 170 \
+--ntp 165 \
 --tr 1 \
---psdwin 0 16 0.1 \
+--psdwin 0 16 0.2 \
 --ev Target 0.5 30 \
 --polyfit 2 \
 --tnullmin 0.5 \
---nsearch 1000 \
+--tsearch 48 \
 --sumdelays \
---nkeep 10 \
+--nkeep 2000 \
 --o seqtest \
 --sum sum.txt
+
+# to create 3 col FSL stim file
+# more seqtest-002.par | grep Target | awk {'print $1 " " $3 " " $2'} > stim2.text
+
+# 3dDeconvolve -nodata 180 1 -polort A -num_stimts 2 -stim_times 1 tt1 'SPMG1' -stim_label 1 Rel -stim_times 2 tt2 'SPMG1' -stim_label 2 IRR
